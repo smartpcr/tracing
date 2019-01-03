efd5b1eab59ac856b314030c15bfe3bcbfa9c6d9
xiaodoli <xiaodoli@microsoft.com>
2019-01-03 15:17:40 -0800

    hello api

---
+++ HelloApi/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Jaeger;
+using Jaeger.Samplers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTracing;
+using OpenTracing.Util;
+
+namespace HelloApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddOpenTracing();
+
+            services.AddSingleton<ITracer>(serviceProvider =>
+            {
+                var serviceName = serviceProvider.GetRequiredService<IHostingEnvironment>().ApplicationName;
+                var tracer = new Tracer.Builder(serviceName).WithSampler(new ConstSampler(true)).Build();
+                GlobalTracer.Register(tracer);
+                return tracer;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
