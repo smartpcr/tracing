2a265e3a88faf6c77df4f5b0a3c2197ba65a58bf
xiaodoli <xiaodoli@microsoft.com>
2019-01-03 19:32:25 -0800

    "clone"

---
+++ clone.cmd
@@ -0,0 +1,5 @@
+git clone https://github.com/opentracing-contrib/csharp-netcore.git csharp-netcore
+git clone https://github.com/jaegertracing/jaeger-client-csharp.git jaeger-client-csharp
+git clone https://github.com/jaegertracing/jaeger-kubernetes.git jaeger-kubernetes
+git clone https://github.com/census-instrumentation/opencensus-csharp.git opencensus-csharp
+git clone https://github.com/opentracing/opentracing-csharp.git opentracing-csharp
